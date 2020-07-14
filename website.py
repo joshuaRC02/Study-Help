8392aa7cc6d68ccf71cefc51c32b3ee6449b9868
Joshua02 <Joshrclark02@hotmail.com>
2020-07-14 18:23:39 -0500

    Update website.py

--- website.py
+++ website.py
@@ -1,7 +1,1 @@
-# pip freeze to get requirements file
-# python -m pip install --upgrade pip
-# py -m pip install --user virtualenv
-# to setup env: py -m venv env
-# .\env\Scripts\activate
-# pip install -r requirements.txt
 from app import app
