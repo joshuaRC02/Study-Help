32372dde4bbff303842bfe4be0efdd38e8c3cc7b
Joshua02 <Joshrclark02@hotmail.com>
2020-07-14 23:22:28 -0500

    Update website.py

--- website.py
+++ website.py
@@ -1,1 +1,8 @@
 from app import app
+import webbrowser
+
+if __name__ == '__main__':
+    # opens the web browser
+    webbrowser.open('http://localhost:5000', new=2, autoraise=True )
+    # runs the app
+    app.run()
